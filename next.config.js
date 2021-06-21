a0fa37962ec958b9e5fccca47cc323f6f0b4ffae
yaromyr.shmid <yshmid@ukrinsoft.net>
2021-06-21 19:11:14 +0300

    Updated next js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
 
 const nextConfig = {
-  future: {
-    webpack5: true
-  },
   images: {
     domains: ['images.ctfassets.net']
   }
